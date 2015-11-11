0d2b91155895bbd5f029e963f0fdb5496eceb6e9
chuck.metzger <chuck.metzger@raymondsci.com>
2015-11-11 11:28:30 -0500

    Add Connection String to Database

--- Company Logo Tool/Form1.cs
+++ Company Logo Tool/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ namespace Company_Logo_Tool
 {
     public partial class mainForm : Form
     {
+        
+        SqlConnection conn = new SqlConnection("data source=RSCI-EBS;initial catalog=TEST;user id=" + Credentials.userName + ";password="+ Credentials.password);
+        SqlCommand command;
+        string imgLoc = "";
+        
         public mainForm()
         {
             InitializeComponent();
